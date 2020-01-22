12704891feb95108281709a55bb7393c0c417d6c
DannySuarez <suarezd10@gmail.com>
2020-01-22 12:56:05 -0800

    optional chaining

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,11 +8,17 @@ export const Input = () => {
   // const [name, setName] = useState<string | null>(null)
 
   // (null!) additional check: ref value is read only
-  const ref = useRef<HTMLInputElement>(null!);
+  const ref = useRef<HTMLInputElement>(null);
   // TypeScript Refs
-  if(ref && ref.current) {
-  console.log(ref.current.value);
-}
+  //optional chaiing ref?.current?
+  //returns falsy value
+  if(ref?.current?.value) {
+    console.log('true', ref.current)
+  } else {
+    console.log('made it to false statement, this is cool', ref.current)
+  }
+  // console.log(ref?.current?.value);
+
   
   return (
     <input ref={ref} value={name} onChange={e => setName(e.target.value)}/>
